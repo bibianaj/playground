a51d06fe732014f80f36d70a7e0ab31ff93f3e13
eunvi <eunvi@LAPTOP-A6RNLGNK>
2021-07-01 21:57:53 +0800

    bibi's playground - Project Initialization

---
+++ src/main/java/com/play/bibi/BibiApplication.java
@@ -0,0 +1,13 @@
+package com.play.bibi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/play/bibi/BibiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.play.bibi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
